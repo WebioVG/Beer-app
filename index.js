3625cc72d5b4821061fdaca4bdead761e58f5401
webiovg <kevin.kowalsky1@gmail.com>
2022-06-13 17:24:29 +0200

    done: display all beers; to be done: single display

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+import VanillaRouter from "/vanillarouter/js/vanillarouter.js"; 
+
+const router = new VanillaRouter({
+    type: "history",
+    routes: {
+        "/": "home",
+        "/search": "search"
+    }
+}).listen().on("route", async e => {
+    
+    // console.log(e.detail.route, e.detail.url);
+
+})
+
 class Header {
     constructor(title) {
         this.title = title
@@ -38,36 +52,124 @@ class Search {
 }
 
 class Results {
-    constructor() {
-        this.display
+    constructor(beersArray) {
+        this.beersArray = beersArray
+        this.display = document.createElement('div')
     }
 
     initialize = () => {
-        const resultsContainer = document.createElement('div')
-        resultsContainer.id = 'resultsContainer'
+        this.display.id = 'resultsContainer'
 
-        document.body.append(resultsContainer)
+        for (let beer of this.beersArray) {
+            beer.initialize()
+        }
+
+        document.body.append(this.display)
     }
 }
 
 class Beer {
-    constructor() {
-        this.display
+    constructor(beer) {
+        this.name = beer.name
+        this.imageUrl = beer.image_url
+
+        this.displayContainer = document.createElement('div')
+        this.displayName = document.createElement('h2')
+        this.displayImage = document.createElement('img')
     }
 
     initialize = () => {
-        const beerContainer = document.createElement('div')
-        beerContainer.id = "beerContainer"
+        this.displayContainer.id = "beerContainer"
+        this.displayName.id = "beerName"
+        this.displayName.textContent = this.name
+        this.displayImage.id = "beerImage"
+        this.displayImage.setAttribute('src', this.imageUrl) 
+        
+        this.displayContainer.append(this.displayName)
+        this.displayContainer.append(this.displayImage)
 
-        resultsContainer.append(beerContainer)
+        results.display.append(this.displayContainer)
     }
 }
+
+class Single {
+    constructor(beer) {
+        this.id = beer.id
+        this.imageUrl = beer.image_url
+        this.name = beer.name
+        this.tips = beer.brewers_tips
+        this.alcoholContent = beer.abv
+        this.foodPairing = beer.food_pairing
+        this.ibu = beer.ibu
+
+        this.displayContainer = document.createElement('div')
+            this.displaySingleContainer = document.createElement('div')
+                this.displayMainContainer = document.createElement('div')
+                    this.displayMainImage = document.createElement('img')
+                    this.displayMainInfoContainer = document.createElement('div')
+                        this.displayMainName = document.createElement('h2')
+                        this.displayMainTips = document.createElement('p')
+                this.displayDetailsContainer = document.createElement('div')
+                    this.displayDetailsInfoContainer = document.createElement('div')
+                        this.displayDetailsAlcoholContent = document.createElement('p')
+                        this.displayDetailsFoodPairing = document.createElement('div')
+                        this.displayDetailsIbu = document.createElement('p')
+                    this.displayDetailsImage = document.createElement('img')
+                this.displayButtonContainer = document.createElement('div')
+                    this.displayPurchaseButton = document.createElement('button')
+
+    }
+
+    initialize = () => {
+        this.displayContainer.id = 'resultsContainer'
+            this.displaySingleContainer.id = 'singleContainer'
+                this.displayMainContainer.id = 'singleMainContainer'
+                    this.displayMainImage.id = 'singleMainImage'
+                    this.displayMainImage.setAttribute('src', this.imageUrl)
+                    this.displayMainInfoContainer.id = 'singleMainInfoContainer'
+                        this.displayMainName.id = 'singleMainName'
+                        this.displayMainName.textContent = this.name
+                        this.displayMainTips.id = 'singleMainTips'
+                        this.displayMainTips.textContent = this.tips
+                this.displayDetailsContainer.id = 'singleDetailsContainer'
+                    this.displayDetailsInfoContainer.id = 'singleDetailsInfoContainer'
+                        this.displayDetailsAlcoholContent.id = 'singleDetailsAlcoholContent'
+                        this.displayDetailsFoodPairing.id = 'singleDetailsFoodPairing'
+                        this.displayDetailsIbu.id = 'singleDetailsIbu'
+                    this.displayDetailsImage.id = 'singleDetailsImage'
+                this.displayButtonContainer.id = 'singleButtonContainer'
+                    this.displayPurchaseButton.id = 'singlePurchaseButton'
+                    this.displayPurchaseButton.textContent = 'Purchase'
+
+
+        this.displayButtonContainer.append(this.displayPurchaseButton)
+        this.displayDetailsContainer.append(this.displayDetailsInfoContainer, this.displayDetailsImage)
+        this.displayMainInfoContainer.append(this.displayMainName, this.displayMainTips)
+        this.displayMainContainer.append(this.displayMainImage, this.displayMainInfoContainer)
+        this.displaySingleContainer.append(this.displayMainContainer, this.displayDetailsContainer, this.displayButtonContainer)
+        this.displayContainer.append(this.displaySingleContainer)
+        document.body.append(this.displayContainer)
+    }
+}
+
+// Initialize variables
+const beersArray = []
+
+// Load the API and store the data
+fetch('https://api.punkapi.com/v2/beers').then(response => {
+    return response.json()
+}).then(data => {
+    data.map(element => {
+        beersArray.push(new Beer(element))
+    })
+    // const single = new Single(data[0])
+    // single.initialize()    
+})
 
 // Create the components
 const header = new Header("Beer App!")
 const search = new Search()
-const results = new Results()
-const beer = new Beer()
+const results = new Results(beersArray)
 
 // Get HTML elements
 const body = document.querySelector('body')
@@ -75,5 +177,9 @@ const body = document.querySelector('body')
 // Load components
 header.initialize()
 search.initialize()
-results.initialize()
-beer.initialize()
+
+
+setTimeout(() => {
+    results.initialize()
+}, 500);
+
